0edc635f23bf0a1faf1876349c7e1b8471dc7325
Mostofareza3 <86896410+Mostofareza3@users.noreply.github.com>
2021-11-11 00:45:57 +0600

    third

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const ObjectId = require("mongodb").ObjectId;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.myaif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-console.log(uri)
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,7 +22,48 @@ client.connect(err => {
     const productCollection = client.db("bikeStore").collection("products");
     const userCollection = client.db("bikeStore").collection("users");
     const reviewCollection = client.db("bikeStore").collection("products");
+    const orderCollection = client.db("bikeStore").collection("myOrders");
     
+
+    //get all products
+    app.get('/products', async(req,res)=>{
+        const result = await productCollection.find({}).toArray();
+        res.send(result)
+    });
+
+    // get specific one
+    app.get('/placeOrder/:id',async (req,res)=>{
+        const id = req.params.id;
+        const result = await productCollection.findOne({_id : ObjectId(id)});
+        res.send(result)
+    })
+
+    //add a product by user
+    app.post('/addOrder',async(req, res)=>{
+        const product = req.body;
+        const result = await orderCollection.insertOne(product)
+        res.json(result)
+    })
+
+    //get all orders
+    app.get('/myOrders',async(req,res)=>{
+
+        const email = req.query.email;
+        const query = {email : email}
+        const result = await orderCollection.find(query).toArray();
+        res.json(result)
+    });
+    //delete order item
+    app.delete('/deleteOrder/:id',async (req,res)=>{
+        console.log(req.query,req.params.id)
+        const id = req.params.id;
+        const email = req.query.email;
+        const result = await orderCollection.deleteOne({
+            email : email  });
+        res.json(result)
+    
+    })
+
     // client.close();
   });
 
